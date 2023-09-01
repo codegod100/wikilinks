565b4f0349537c43edaa687973b8d15ca909d633
V <veronika.m.winters@gmail.com>
2023-08-31 19:05:45 -0700

    nice

--- content.tsx
+++ content.tsx
@@ -3,19 +3,28 @@ import * as $ from "jquery"
 import { useStorage } from "@plasmohq/storage/hook"
 
 function render(url) {
+  console.log("render")
   $('p:contains("[[")').map(function () {
     let text = this.innerHTML
-    text = text.replace(/\[\[(.*?)\]\]/g,`[[<a href='${url}/$1'>$1</a>]]`)
+    text = text.replace(
+      /\[\[([a-zA-Z0-9  -]+?)\]\]/g,
+      `[[<a href='${url}/$1'>$1</a>]]`
+    )
     this.innerHTML = text
   })
 }
-
-  function Content() {
-    let [url] = useStorage("url")
-    if(!url){return }
-    setTimeout(() => render(url), 1000)
+let interval
+function Content() {
+  if (interval) {
+    clearInterval(interval)
+  }
+  let [url] = useStorage("url")
+  console.log({ url })
+  // url = url || "https://anagora.org"
+  if (url) {
+    interval = setInterval(() => render(url), 1000)
+  }
   return <span></span>
 }
 
 export default Content
-
