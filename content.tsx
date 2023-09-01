16ff3f472a79705f478050753c959d2b9b589b78
V <veronika.m.winters@gmail.com>
2023-08-31 18:23:04 -0700

    cleanups

--- content.tsx
+++ content.tsx
@@ -1,32 +1,22 @@
 import * as $ from "jquery"
 
-import Wikilink from "./wikilink"
 import ReactDomServer from 'react-dom/server';
 import { useStorage } from "@plasmohq/storage/hook"
 
-function render(wikilink) {
+function render(url) {
   $('p:contains("[[")').map(function () {
     let text = this.innerHTML
-    text = text.replace(/\[\[(.*?)\]\]/g, ReactDomServer.renderToString(wikilink))
+    text = text.replace(/\[\[(.*?)\]\]/g,`[[<a href='${url}/$1'>$1</a>]]`)
     this.innerHTML = text
   })
 }
 
-
-function Wikilink({url}){
-  url = `${url}/$1`
-  return (
-    <span>[[<a href={url}>$1</a>]]</span>
-    )
-  }
-  
-  function foo() {
+  function Content() {
     let [url] = useStorage("url")
     if(!url){return }
-    let wikilink = <Wikilink url={url} />
-    setTimeout(() => render(wikilink), 100)
+    setTimeout(() => render(url), 1000)
   return <span></span>
 }
 
-export default foo
+export default Content
 
